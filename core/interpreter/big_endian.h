31bffa898e96a2ebe5cbdafaced00b15eddba88c
Timo Kloss <timo@inutilis.com>
2017-04-10 22:25:07 +0200

    renamed project to LowRes NX.

--- core/interpreter/big_endian.h
+++ core/interpreter/big_endian.h
@@ -1,20 +1,20 @@
 //
 // Copyright 2017 Timo Kloss
 //
-// This file is part of LowRes Core.
+// This file is part of LowRes NX.
 //
-// LowRes Core is free software: you can redistribute it and/or modify
+// LowRes NX is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// LowRes Core is distributed in the hope that it will be useful,
+// LowRes NX is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with LowRes Core.  If not, see <http://www.gnu.org/licenses/>.
+// along with LowRes NX.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #ifndef big_endian_h
