1d8d63f5cbc6732a4555ee2080079608bab8c53e
Timo Kloss <timo@inutilis.com>
2018-07-28 15:45:10 +0200

    Removed non-SDL version for macOS.

--- platform/LowRes NX macOS/LowRes NX macOS/LowResNX-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  LowResNX-Bridging-Header.h
-//  LowRes NX macOS
-//
-//  Created by Timo Kloss on 23/4/17.
-//  Copyright © 2017 Inutilis Software. All rights reserved.
-//
-
-#include "core.h"
